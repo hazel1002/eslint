7e4641053ef2956f55ba50d9ca73fc20d8bfe5c1
Hazel Huang <hazel.huang@shinda.com.tw>
2024-05-28 15:44:33 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = "my-app";
+  getData() {
+    const a = 3;
+    //console.log(a);
+  }
 }
